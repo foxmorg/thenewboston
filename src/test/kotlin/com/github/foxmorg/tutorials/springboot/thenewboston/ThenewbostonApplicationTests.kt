49d678748cd9308e0ea9efdcd8a680d71471dc57
foxmorg <foxmorg@gmail.com>
2022-04-28 00:40:22 +0300

    tutorial 1 & 2

---
+++ src/test/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/ThenewbostonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.foxmorg.tutorials.springboot.thenewboston
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThenewbostonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
