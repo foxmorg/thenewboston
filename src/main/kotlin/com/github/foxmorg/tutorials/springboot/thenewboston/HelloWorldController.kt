0f7109201b9c814c7031f2481a9974ad027e5d7c
foxmorg <foxmorg@gmail.com>
2022-04-29 20:40:43 +0300

    line separators changed from win to unix

--- src/main/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/HelloWorldController.kt
+++ src/main/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/HelloWorldController.kt
@@ -1,13 +1,1 @@
-package com.github.foxmorg.tutorials.springboot.thenewboston
-
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@RequestMapping("api/hello")
-class HelloWorldController {
-
-    @GetMapping
-    fun helloWorld(): String = "Hello, this is a REST endpoint!"
-}
+package com.github.foxmorg.tutorials.springboot.thenewbostonimport org.springframework.web.bind.annotation.GetMappingimport org.springframework.web.bind.annotation.RequestMappingimport org.springframework.web.bind.annotation.RestController@RestController@RequestMapping("api/hello")class HelloWorldController {    @GetMapping    fun helloWorld(): String = "Hello, this is a REST endpoint!"}
