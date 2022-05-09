9079fae21787af3c2ddb77c3ead57a3bfb45c2fd
foxmorg <foxmorg@gmail.com>
2022-05-09 18:41:27 +0300

    tutorial 9 - POST Endpoint

--- src/main/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/datasource/BankDataSource.kt
+++ src/main/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/datasource/BankDataSource.kt
@@ -5,4 +5,5 @@ import com.github.foxmorg.tutorials.springboot.thenewboston.model.Bank
 interface BankDataSource {
     fun retrieveBanks(): Collection<Bank>
     fun retrieveBank(accountNumber: String): Bank
+    fun createBank(bank: Bank): Bank
 }
