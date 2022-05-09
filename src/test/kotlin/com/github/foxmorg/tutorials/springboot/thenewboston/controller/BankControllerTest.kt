74bf64c7a5192b9a7fdbb0c6da064d5f1e4b1219
foxmorg <foxmorg@gmail.com>
2022-05-09 17:03:59 +0300

    tutorial 8 - GET single bank

--- src/test/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
+++ src/test/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
@@ -1,7 +1,11 @@
 package com.github.foxmorg.tutorials.springboot.thenewboston.controller
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
@@ -16,16 +20,61 @@ internal class BankControllerTest {
     @Autowired
     lateinit var mockMvc: MockMvc
 
-    @Test
-    fun `should return all banks`() {
-        // when/then
-        mockMvc.get("/api/banks")
-                .andDo { print() }
-                .andExpect {
-                    status { isOk() }
-                    content { contentType(MediaType.APPLICATION_JSON) }
-                    jsonPath("$[0].accountNumber") { value("1234") }
-                }
+    val baseUrl = "/api/banks"
+
+    @Nested
+    @DisplayName("getBanks()")
+    @TestInstance(Lifecycle.PER_CLASS)
+    inner class GetBanks {
+
+        @Test
+        fun `should return all banks`() {
+            // when/then
+            mockMvc.get(baseUrl)
+                    .andDo { print() }
+                    .andExpect {
+                        status { isOk() }
+                        content { contentType(MediaType.APPLICATION_JSON) }
+                        jsonPath("$[0].accountNumber") { value("1234") }
+                    }
+        }
+    }
+
+    @Nested
+    @DisplayName("getBank()")
+    @TestInstance(Lifecycle.PER_CLASS)
+    inner class GetBank {
+
+        @Test
+        fun `should return the bank with the given account number`() {
+            // given
+            val accountNumber = 1234
+
+            // when/then
+            mockMvc.get("$baseUrl/$accountNumber")
+                    .andDo { print() }
+                    .andExpect {
+                        status { isOk() }
+                        content { contentType(MediaType.APPLICATION_JSON) }
+                        jsonPath("$.trust") { value("3.14") }
+                        jsonPath("$.transactionFee") { value("17") }
+                    }
+        }
+
+        @Test
+        fun `should return NOT FOUND id the account number does not exist`() {
+            // given
+            val accountNumber = "does_not_exist"
+
+            // when/then
+            mockMvc.get("$baseUrl/$accountNumber")
+                    .andDo { print() }
+                    .andExpect {
+                        status { isNotFound() }
+                    }
+
+        }
+
     }
 
 }
