d2bd848098d5b8c5ae4c2765db29bff0d1c632af
foxmorg <foxmorg@gmail.com>
2022-05-10 20:47:46 +0300

    tutorial 12 - Testing best practices

--- src/test/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
+++ src/test/kotlin/com/github/foxmorg/tutorials/springboot/thenewboston/controller/BankControllerTest.kt
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.delete
 import org.springframework.test.web.servlet.get
@@ -191,6 +192,7 @@ internal class BankControllerTest @Autowired constructor(
     inner class DeleteExistingBank {
 
         @Test
+        @DirtiesContext
         fun `should delete the bank with the given account number`() {
             // given
             val accountNumber = "1234"
